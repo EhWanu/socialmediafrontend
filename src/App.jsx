c0de981a87c5ded9a25eac7c70f9137078848a02
EhWanu <cameronjshadgett@gmail.com>
2021-11-02 15:29:25 -0400

    setup folder structure and created home componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Home from "./pages/home/Home";
+
 function App() {
-	return <div className="app"></div>;
+	return (
+		<div className="app">
+			<Home />
+		</div>
+	);
 }
 
 export default App;

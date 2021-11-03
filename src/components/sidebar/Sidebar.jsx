3a8911bcc70d924dd673a32e9217f8d07cef208a
EhWanu <cameronjshadgett@gmail.com>
2021-11-03 13:06:24 -0400

    created sidebar lists with material icons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,84 @@
 import "./sidebar.css";
+import {
+	RssFeed,
+	Bookmark,
+	Chat,
+	Group,
+	HelpOutline,
+	School,
+	VideoLibrary,
+	WorkOutlineOutlined,
+	Event,
+} from "@material-ui/icons";
 
 export default function Sidebar() {
-	return <div className="sidebar">sidebar</div>;
+	return (
+		<div className="sidebar">
+			<div className="sidebarWrapper">
+				<ul className="sidebarList">
+					<li className="sidebarListItem">
+						<RssFeed className="sidebarIcon" />
+						<span className="sidebarListItemText">
+							Feed
+						</span>
+					</li>
+
+					<li className="sidebarListItem">
+						<Chat className="sidebarIcon" />
+						<span className="sidebarListItemText">
+							Chat
+						</span>
+					</li>
+					<li className="sidebarListItem">
+						<VideoLibrary className="sidebarIcon" />
+						<span className="sidebarListItemText">
+							Videos
+						</span>
+					</li>
+					<li className="sidebarListItem">
+						<Group className="sidebarIcon" />
+						<span className="sidebarListItemText">
+							groups
+						</span>
+					</li>
+					<li className="sidebarListItem">
+						<Bookmark className="sidebarIcon" />
+						<span className="sidebarListItemText">
+							Feed
+						</span>
+					</li>
+					<li className="sidebarListItem">
+						<HelpOutline className="sidebarIcon" />
+						<span className="sidebarListItemText">
+							Feed
+						</span>
+					</li>
+					<li className="sidebarListItem">
+						<WorkOutlineOutlined className="sidebarIcon" />
+						<span className="sidebarListItemText">
+							Feed
+						</span>
+					</li>
+					<li className="sidebarListItem">
+						<WorkOutlineOutlined className="sidebarIcon" />
+						<span className="sidebarListItemText">
+							Feed
+						</span>
+					</li>
+					<li className="sidebarListItem">
+						<Event className="sidebarIcon" />
+						<span className="sidebarListItemText">
+							Feed
+						</span>
+						<li className="sidebarListItem">
+							<School className="sidebarIcon" />
+							<span className="sidebarListItemText">
+								Feed
+							</span>
+						</li>
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
